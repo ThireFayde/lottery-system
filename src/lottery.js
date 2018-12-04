5b9a2d4c15df4e02ad238b90491774133d222f1b
原田 拓磨 <t.harada@april-knights.com>
2018-12-05 01:16:58 +0900

    no message

---
+++ src/lottery.js
@@ -0,0 +1,14 @@
+export default class Lottery {
+
+    //コンストラクタ
+    constructor() {
+    }
+    sayHello() {
+        const hello = 'helloworld';
+        const test = () => {
+            console.log('helloworld');
+        } 
+        test();
+        return hello;
+    }
+}
